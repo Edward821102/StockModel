7d383860a8d7c2e6d5293796d926a083d5bcdba6
MSI\Edwar <edward821103@gmail.com>
2024-08-06 19:33:56 +0800

    Update README.md, line_bot.py, and 2 more files...

--- api/line_bot.py
+++ api/line_bot.py
@@ -1,62 +1,12 @@
-# from http.server import BaseHTTPRequestHandler
-# import json
-# import os
-# from linebot import LineBotApi, WebhookHandler
-# from linebot.exceptions import InvalidSignatureError
-# from linebot.models import MessageEvent, TextMessage, TextSendMessage
-
-# LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
-# LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
-
-# line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-# webhook_handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-# class handler(BaseHTTPRequestHandler):
-#     def do_POST(self):
-#         signature = self.headers.get('X-Line-Signature')
-#         content_length = int(self.headers['Content-Length'])
-#         body = self.rfile.read(content_length).decode('utf-8')
-
-#         try:
-#             handler.handle(body, signature)
-#             self.send_response(200)
-#         except InvalidSignatureError:
-#             self.send_response(400)
-
-#         self.send_header('Content-Type', 'text/plain; charset=utf-8')
-#         self.end_headers()
-
-#     def do_GET(self):
-#         self.send_response(200)
-#         self.send_header('Content-Type', 'text/plain; charset=utf-8')
-#         self.end_headers()
-#         self.wfile.write(b'Welcome Edward\'s Palace.')
-
-#     @webhook_handler.add(MessageEvent, message=TextMessage)
-#     def handle_message(event):
-#         user_message = event.message.text
-#         response = execute_python_logic(user_message)
-
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(text=response)
-#         )
-
-# def execute_python_logic(user_message):
-#     if user_message.lower() == "hi":
-#         return "Hello! How can I help you today?"
-#     elif user_message.lower() == "bye":
-#         return "Goodbye! Have a nice day!"
-#     else:
-#         return f"你說的都對：{user_message}"
-    
 from http.server import BaseHTTPRequestHandler
-import os
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from stock_model.fetch_twstock import WebCrawlerStockVolumes
+from stock_model.neuron import Neuron
+from twstock import Stock
+import os
 
-# 从环境变量获取 Line 的凭证
 LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
 LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
 
@@ -96,10 +46,22 @@ class handler(BaseHTTPRequestHandler):
     @webhook_handler.add(MessageEvent, message=TextMessage)
     def handle_message(event):
         user_message = event.message.text
-        response = f"你傳了這是什麼鬼?{user_message}"
-
-        # 发送回复消息
+        response = get_close(user_message)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=response)
         )
+    
+def get_close(stock_id:str) -> str:
+    wcsv = WebCrawlerStockVolumes(stock_id)
+    vol = int(wcsv.get_volumes())
+    stock = Stock(stock_id)
+    data = stock.fetch_from(2018, 1)
+    ohlcv_list = [(i.open / 1000, i.high / 1000, i.low / 1000, i.close / 1000, i.capacity / (vol * 1000)) for i in data]
+    target_list = [i[3] for i in ohlcv_list]
+    train_samples = ohlcv_list[:-30]
+    train_targets = target_list[1:-29]
+    neuron = Neuron()
+    loss = neuron.train(train_samples, train_targets)
+    prediction = neuron.predict(ohlcv_list[-1])
+    return f"Loss:{loss}, 隔日收盤價:{prediction * 1000}"
