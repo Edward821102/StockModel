20ab3d86b9db98a69bd2be00be9bc33c93354942
MSI\Edwar <edward821103@gmail.com>
2024-08-06 16:56:58 +0800

    fix line bot

--- api/line_bot.py
+++ api/line_bot.py
@@ -9,9 +9,20 @@ LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
 LINE_CHANNEL_SECRET = os.environ['LINE_CHANNEL_SECRET']
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
+web_hook_handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-class Handler(BaseHTTPRequestHandler):
+from http.server import BaseHTTPRequestHandler
+
+# class handler(BaseHTTPRequestHandler):
+
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/plain')
+#         self.end_headers()
+#         self.wfile.write('Hello, world!'.encode('utf-8'))
+#         return
+
+class handler(BaseHTTPRequestHandler):
     def do_POST(self):
 
         signature = self.headers['X-Line-Signature']
@@ -28,7 +39,7 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
 
-    @handler.add(MessageEvent, message=TextMessage)
+    @web_hook_handler.add(MessageEvent, message=TextMessage)
     def handle_message(event):
         user_message = event.message.text
         response = execute_python_logic(user_message)
