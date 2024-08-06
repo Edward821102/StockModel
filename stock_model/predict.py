ea975da7905e4f9e73130c3918c7212a48ecd781
MSI\Edwar <edward821103@gmail.com>
2024-08-06 19:33:56 +0800

    Update README.md, line_bot.py, and 2 more files...

--- stock_model/predict.py
+++ stock_model/predict.py
@@ -15,21 +15,3 @@ class Model:
         prediction = self.neuron.predict(samples)
         return list(prediction)[0] * 1000
     
-if __name__ == "__main__":
-    wcsv = WebCrawlerStockVolumes("2330")
-    vol = int(wcsv.get_volumes())
-    stock = Stock("2330")
-    data = stock.fetch_from(2018, 1)
-    ohlcv_list = [(i.open / 1000, i.high / 1000, i.low / 1000, i.close / 1000, i.capacity / (vol * 1000)) for i in data]
-    target_list = [i[3] for i in ohlcv_list]
-    train_samples = ohlcv_list[:-30]
-    valid_samples = ohlcv_list[-30:-1]
-    train_targets = target_list[1:-29]
-    valid_tragets = target_list[-29:]
-    neuron = Neuron()
-    loss = neuron.train(train_samples, train_targets)
-    prediction = neuron.predict(ohlcv_list[-1])
-    print(loss, prediction * 1000)
-    # print(ohlcv_list[-1])
-    # print("*"*50)
-    # print(prediction)
